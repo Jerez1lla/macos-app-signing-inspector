3a72ce7bda90499034d263bc92cd13b6cbfef98f
Jerez1lla <304531088+Jerez1lla@users.noreply.github.com>
2026-07-13 12:14:39 -0400

    Initialize App Signing Inspector project

---
+++ AppSigningInspectorTests/AppSigningInspectorTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import AppSigningInspector
+
+final class AppSigningInspectorTests: XCTestCase {
+    func testInitialProjectLoadsContentView() {
+        _ = ContentView()
+    }
+}
